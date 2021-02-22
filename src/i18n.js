58f99f9ab75e7106479f9e34a13d65e2958b2e9f
chinicuil <soychinicuil@outlook.com>
2021-02-22 12:44:24 +0100

    test nuevo

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+//https://github.com/i18next/i18next-xhr-backend/blob/master/README.md#backend-options
 
 i18n
   // load translation using http -> see /public/locales
